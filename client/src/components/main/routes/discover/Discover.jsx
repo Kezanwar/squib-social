2d1b168f948bcc154f986707c0248618d0081e45
Kez Anwar <Kezanwar@gmail.com>
2022-05-10 20:03:37 +0100

    more work on likes and comments

--- client/src/components/main/routes/discover/Discover.jsx
+++ client/src/components/main/routes/discover/Discover.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
 import RouteWrapper from '../../../layout/RouteWrapper'
+import Loading from '../../Loading'
 
 const Discover = () => {
-  return <RouteWrapper id={'discover'} className={'discover'}></RouteWrapper>
+  return (
+    <RouteWrapper id={'discover'} className={'discover'}>
+      <Loading />
+    </RouteWrapper>
+  )
 }
 
 export default Discover
